72318b2f17cf5601b1b40dc5a07ac8c9324ddb3c
blob <adrianwylo1@gmail.com>
2023-08-23 02:46:08 -0500

    added directions (untetsted)

--- paintings/color_mono.py
+++ paintings/color_mono.py
@@ -2,13 +2,21 @@ import cv2
 import board
 import neopixel
 
-def shift_led_image(c_dic, p_dic, b, a):
+def shift_led_image(c_dic, p_dic, b, a, tilted):
     total_steps = 20  # changeable
     for step in range(total_steps + 1):
         for y in range(b):
             for x in range(a):
-                pixels[p_dic[(y, x)]] = interpolate_color(c_dic[(y, x)][0], c_dic[(y, x)][1], step, total_steps)
-        pixels.show()
+                color = interpolate_color(c_dic[(y, x)][0], c_dic[(y, x)][1], step, total_steps)
+                if tilted == "left":
+                    pixels[p_dic[(b - x, y)]] = color
+                elif tilted == "right":
+                    pixels[p_dic[(x, a - y)]] = color
+                elif tilted == "up":
+                    pixels[p_dic[(b - y, a - x)]] = color
+                elif tilted == "down":
+                    pixels[p_dic[(y, x)]] = color
+    pixels.show()
 
 # helper Function to interpolate between two colors
 def interpolate_color(color2, color1, step, total_steps):
@@ -33,13 +41,13 @@ def update_next_colors(frame, c_dic, b, a, tint_color):
             r, g, b = frame[int(y), int(x)]
             # Convert to grayscale using luminance formula: 0.2989 * R + 0.5870 * G + 0.1140 * B
             gb = int(0.2989 * r + 0.5870 * g + 0.1140 * b)
-            
             c_dic[(y, x)] = (tint_grayscale(gb, tint_color), 
                              (c_dic[(y, x)][0][0], c_dic[(y, x)][0][1], c_dic[(y, x)][0][2]))
             
             
 
-def create_mapping(mapping, color_bank, b, a, panel_size):
+def create_mapping(mapping, color_bank, b, a, panel_size, display_mode):
+    #base mapping
     for y in range(b):
         for x in range(a):
             y_panel = int(y // panel_size)
@@ -52,19 +60,20 @@ def create_mapping(mapping, color_bank, b, a, panel_size):
             else:
                 offset_amount = (x_panel_offset + 1) * panel_size - 1 - y_panel_offset
             pixel_index = base_pixel + offset_amount
-            mapping[(y, x)] = pixel_index
+            if display_mode == "normal":
+                mapping[(y, b - x - 1)] = pixel_index
+            else:  #mirrored
+                mapping[(y, x)] = pixel_index
             color_bank[(y, x)] = ((0, 0, 0), (0, 0, 0))
+                
 
 x = 64 #X dimension of panel
 y = 32 #Y dimension of panel
 panel_size = 16 #dimension of panel
 NUM_PIXELS = x * y
 
-# Define the cropping region's top-left corner and dimensions
-crop_x = 100
-crop_y = 100
-crop_width = 200
-crop_height = 200
+display_mode = "normal" #Options: "normal", "mirrored"
+tilted = "left" #Options: "normal", "left", "right", "up"
 
 cam_index = 0 #cam index
 
@@ -78,8 +87,9 @@ cap = cv2.VideoCapture(cam_index)
 pos_dictionary = {}
 col_dictionary = {}
 
-#map coordinates to pixel #
-create_mapping(pos_dictionary, col_dictionary, y, x, panel_size)
+#map coordinates to pixel 
+
+create_mapping(pos_dictionary, col_dictionary, y, x, panel_size, display_mode)
 
 while True:
     #capture pic
@@ -87,19 +97,15 @@ while True:
     if not ret:
         print("Capture error")
         break
-    
-    #crop/resize frame
-    # Crop the frame to the defined region
-    cropped_frame = frame[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
 
     # Resize cropped frame to match panel dimensions
-    frame = cv2.resize(cropped_frame, (x, y))
+    frame = cv2.resize(frame, (x, y))
 
     #process frame
     update_next_colors(frame, col_dictionary, y, x, tint_color)
 
     #display
-    shift_led_image(col_dictionary, pos_dictionary, y, x)
+    shift_led_image(col_dictionary, pos_dictionary, y, x, tilted)
     
 
 cap.release()
