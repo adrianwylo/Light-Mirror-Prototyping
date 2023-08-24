894eb3c767e67fc7a12e436281f9feee2c8f52d3
blob <adrianwylo1@gmail.com>
2023-08-23 22:39:51 -0500

    program runner2

--- paintings/runner.py
+++ paintings/runner.py
@@ -1,8 +1,22 @@
 import multiprocessing
 import os
+import time
 
 def run_script(script_name):
-    os.system(f"sudo python3 {script_name}")  # Replace with the appropriate command to run your scripts
+    while True:
+        try:
+            print(f"Running {script_name}")
+            exit_code = os.system(f"sudo python3 {script_name}")  # Replace with the appropriate command to run your scripts
+            
+            if exit_code == 0:
+                print(f"{script_name} completed successfully.")
+            else:
+                print(f"{script_name} encountered an error. Restarting...")
+            
+            time.sleep(5)  # Wait for a while before restarting the script
+        except Exception as e:
+            print(f"An exception occurred while running {script_name}: {e}. Restarting...")
+            time.sleep(5)  # Wait for a while before restarting the script
 
 if __name__ == "__main__":
     scripts_to_run = ["script1.py", "script2.py", "script3.py"]  # List of scripts you want to run
@@ -15,5 +29,3 @@ if __name__ == "__main__":
 
     for process in processes:
         process.join()
-
-    print("All scripts have finished.")
